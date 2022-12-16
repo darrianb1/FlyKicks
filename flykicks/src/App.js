cf3aaebc80a1753cc279f239f6bd5e0c5eccf5db
Darrian Benford <dbenford2@art.edu>
2022-12-16 08:49:53 -0600

    Start of ecommerce

---
+++ flykicks/src/App.js
@@ -0,0 +1,18 @@
+
+
+
+const App = () => {
+  return (
+    <div className="categories-container">
+    
+      <div className="category-container">
+        <div className="categoty-container-body">
+          <h2>Space Flyers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
